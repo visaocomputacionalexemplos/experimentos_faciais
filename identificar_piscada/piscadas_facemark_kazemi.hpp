9f56f0a046368d46ee969ee9103e7fb5f8559119
Piemontez <piemontez@live.com>
2021-10-12 19:38:56 -0300

    rename files

--- identificar_piscada/piscadas_facemark_kazemi.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef PISCADAS_FACEMARKL_KAZEMI_H
-#define PISCADAS_FACEMARKL_KAZEMI_H
-
-#include "opencv2/face.hpp"
-
-cv::Ptr<cv::face::Facemark> initFacemarkKazemi();
-
-#endif //PISCADAS_FACEMARKL_KAZEMI_H
--- identificar_piscada/piscadas_facemark_lbf.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef PISCADAS_FACEMARKL_LBF_H
-#define PISCADAS_FACEMARKL_LBF_H
-
-#include "opencv2/face.hpp"
-
-cv::Ptr<cv::face::Facemark> initFacemarkLBF();
-
-#endif //PISCADAS_FACEMARKL_LBF_H
